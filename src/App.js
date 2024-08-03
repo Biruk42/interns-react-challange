0a901eea91f401ccf568ab82292732ece6ac3cf9
Biruk Kassaye <kassayebiruk0@gmail.com>
2024-08-03 11:31:48 +0300

    Add the loader and error in the app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import ActorCard from "./components/ActorCard";
+import ActorDetail from "./components/ActorDetail";
+import Loading from "./components/Loading";
+import Error from "./components/Error";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [actors, setActors] = useState([]);
+  const [selectedActor, setSelectedActor] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchActors = async () => {
+      try {
+        const response = await fetch("https://swapi.py4e.com/api/people/");
+        const data = await response.json();
+        setActors(data.results);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchActors();
+  }, []);
+
+  if (loading) return <Loading />;
+  if (error) return <Error message={error} />;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 className="title">Starwars Actors</h1>
+      <div className={`actor-list ${selectedActor ? "blur-background" : ""}`}>
+        {actors.map((actor) => (
+          <ActorCard
+            key={actor.name}
+            actor={actor}
+            onDetailClick={() => setSelectedActor(actor)}
+          />
+        ))}
+      </div>
+      {selectedActor && (
+        <div className="detail-wrapper">
+          <ActorDetail
+            actor={selectedActor}
+            onClose={() => setSelectedActor(null)}
+          />
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
